3ced8fd3a43ef58ae1e83e8e59b44c4690a33251
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2026-05-29 18:25:52 +0400

    ux: remove Aujourd'hui from calendar legend

--- src/pages/Calendrier/CalendrierPage.jsx
+++ src/pages/Calendrier/CalendrierPage.jsx
@@ -539,7 +539,6 @@ const CalendrierPage = () => {
         <span className="calendar-legend-item"><span className="legend-dot bg-danger"></span>Refusé</span>
         <span className="calendar-legend-item"><span className="legend-dot bg-primary"></span>Absence</span>
         <span className="calendar-legend-item"><span className="legend-dot legend-dot-ferie"></span>Jour férié</span>
-        <span className="calendar-legend-item"><span className="legend-dot legend-dot-today"></span>Aujourd'hui</span>
       </div>
 
       <Modal show={showEventDetailsModal} onHide={closeEventDetailsModal} centered>
